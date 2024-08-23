a11632fc1e680a0541e09e9ef33f256f52d21176
Tojo Ny Aina <Itstojonyaina007@gmail.com>
2024-08-23 11:04:24 +0300

    Mandamina

---
+++ Distance_de_Levenshtein/Distance_de_Levenshtein/ChargementMotDansDico.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Question 2
+namespace Distance_de_Levenshtein
+{
+    internal class ChargementMotDansDico
+    {
+        static Dictionary<string, bool> ChargerMotsDansDictionnaire(string cheminFichier)
+        {
+            Dictionary<string, bool> dictionnaireMots = new Dictionary<string, bool>();
+
+            try
+            {
+                foreach (string ligne in File.ReadLines(cheminFichier))
+                {
+                    string mot = ligne.Trim();
+
+                    if (!string.IsNullOrEmpty(mot) && !dictionnaireMots.ContainsKey(mot))
+                    {
+                        dictionnaireMots.Add(mot, true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors de la lecture du fichier : {ex.Message}");
+                return null;
+            }
+
+            return dictionnaireMots;
+        }
+    }
+}
---
+++ Distance_de_Levenshtein/Distance_de_Levenshtein/Levenshtein.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance_de_Levenshtein
+{
+    internal class Levenshtein
+    {
+        int i, j = 0;
+        
+    }
+}
